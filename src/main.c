664fdc3a137de91d8b29b3b86b546071b9847412
SYGalvinChan <sygalvinchan@gmail.com>
2023-12-21 13:19:51 +0800

    Change exception level to 1

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include "console.h"
 #include "printf.h"
+#include <stdint.h>
+
+int current_EL();
 
 void main(void) {
 
 	console_init();
 	console_write("Console initialized!!!!\r\n");
+	printf("Current Exception Level: %d\r\n", current_EL());
 
 	while (1) {
 		volatile int i = 0;
@@ -13,4 +17,11 @@ void main(void) {
 		}
 		console_write(".");
 	}
+}
+
+int current_EL() {
+	int register_value;
+	asm ("mrs %0, CurrentEL" : "=r" (register_value));
+	register_value = register_value >> 2;
+	return register_value;
 }
