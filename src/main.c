047cde9220e33a5ef8bc5201240b3e54de00d36b
SYGalvinChan <sygalvinchan@gmail.com>
2024-01-02 16:19:37 +0800

    kernel boots into multi core

--- src/main.c
+++ src/main.c
@@ -9,22 +9,36 @@ void irq_init_vectors();
 void irq_enable();
 void irq_disable();
 
+char cpu_stacks[4 * 4096];
+int started = 0;
+
+int cpu_id();
+
 void main(void) {
+	if (cpu_id() == 0) {
+		console_init();
+		console_write("Console initialized!!!!\r\n");
 
-	console_init();
-	console_write("Console initialized!!!!\r\n");
-
-	irq_init_vectors();
-	system_timer_init();	
-	interupt_controller_init();	
-	exception_handler_init();
-	irq_enable();
-
+		// irq_init_vectors();
+		// system_timer_init();	
+		// interupt_controller_init();	
+		// exception_handler_init();
+		// irq_enable();
+		started = 1;
+	}
+	
+	while (started == 0) ;
 	while (1) {
 		volatile int i = 0;
 		while (i < 1000000) {
 			i++;
 		}
-		printf(".");
+		printf("%d", cpu_id());
 	}
+}
+
+int cpu_id() {
+	uint32_t register_value;
+	asm ("mrs %0, mpidr_el1" : "=r" (register_value));
+	return register_value & 3;
 }
